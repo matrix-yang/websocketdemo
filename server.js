2c7f1bc96c1df82af9c5421f082b8c1473b68007
matrix-yang <739260804@qq.com>
2018-04-28 11:52:08 +0800

    2个聊天室

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 //https://github.com/websockets/ws/blob/master/doc/ws.md#new-wsserveroptions-callback
 const http = require('http');
 const WebSocket = require('ws');
-
-const server = http.createServer();
-const wss1 = new WebSocket.Server({ noServer: true });
-const wss2 = new WebSocket.Server({ noServer: true });
-
-const http = require('http');
-const WebSocket = require('ws');
+const url=require('url');
 
 const server = http.createServer();
 const wss1 = new WebSocket.Server({ noServer: true });
@@ -32,14 +26,14 @@ wss1.on('connection', function connection(ws) {
     //console.log(ws.clients.session);
     // console.log(wss.clients.size)
     // console.log("在线人数", wss.clients.length);
-    ws.send('这是聊天室1，你是第' + wss.clients.size + '位');
+    ws.send('这是聊天室1，你是第' + wss1.clients.size + '位');
     // 发送消息
     ws.on('message', function(jsonStr,flags) {
         var obj = eval('(' + jsonStr + ')');
         // console.log(obj);
         this.user = obj;
         if (typeof this.user.msg != "undefined") {
-            wss.broadcast(1,obj);
+            wss1.broadcast(1,obj);
         }
     });
     // 退出聊天
@@ -71,20 +65,20 @@ wss2.on('connection', function connection(ws) {
     //console.log(ws.clients.session);
     // console.log(wss.clients.size)
     // console.log("在线人数", wss.clients.length);
-    ws.send('这是聊天室2，你是第' + wss.clients.size + '位');
+    ws.send('这是聊天室2，你是第' + wss2.clients.size + '位');
     // 发送消息
     ws.on('message', function(jsonStr,flags) {
         var obj = eval('(' + jsonStr + ')');
         // console.log(obj);
         this.user = obj;
         if (typeof this.user.msg != "undefined") {
-            wss.broadcast(1,obj);
+            wss2.broadcast(1,obj);
         }
     });
     // 退出聊天
     ws.on('close', function(close) {
         try{
-            wss.broadcast(0,this.user.name);
+            wss2.broadcast(0,this.user.name);
         }catch(e){
             console.log('刷新页面了');
         }
@@ -92,8 +86,8 @@ wss2.on('connection', function connection(ws) {
 });
 
 server.on('upgrade', function upgrade(request, socket, head) {
+    //console.log(request.getParameter('group'))
     const pathname = url.parse(request.url).pathname;
-
     if (pathname === '/foo') {
         wss1.handleUpgrade(request, socket, head, function done(ws) {
             wss1.emit('connection', ws);
@@ -106,46 +100,4 @@ server.on('upgrade', function upgrade(request, socket, head) {
         socket.destroy();
     }
 });
-
 server.listen(3000);
-
-
-//广播  
-wss.broadcast = function broadcast(s,ws) {
-    // console.log(ws);  
-    // debugger;  
-    wss.clients.forEach(function each(client) {
-        // if (typeof client.user != "undefined") {  
-        if(s == 1){
-            client.send(ws.name + ":" + ws.msg);
-        }
-        if(s == 0){
-            client.send(ws + "退出聊天室");
-        }
-        // }  
-    });
-};
-// 初始化  
-wss.on('connection', function(ws) {
-    //console.log(ws.clients.session);
-    // console.log(wss.clients.size)
-    // console.log("在线人数", wss.clients.length);  
-    ws.send('你是第' + wss.clients.size + '位');
-    // 发送消息  
-    ws.on('message', function(jsonStr,flags) {
-        var obj = eval('(' + jsonStr + ')');
-        // console.log(obj);  
-        this.user = obj;
-        if (typeof this.user.msg != "undefined") {
-            wss.broadcast(1,obj);
-        }
-    });
-    // 退出聊天  
-    ws.on('close', function(close) {
-        try{
-            wss.broadcast(0,this.user.name);
-        }catch(e){
-            console.log('刷新页面了');
-        }
-    });
-});  
